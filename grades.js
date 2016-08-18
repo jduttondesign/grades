5af932de26d2c08ec8d38c7d55e910ec3bdae707
Jennifer Dutton <jduttondesign@gmail.com>
2016-08-17 23:43:57 -0500

    added index and js files

---
+++ grades.js
@@ -0,0 +1,54 @@
+grades.js
+
+var grades = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var aGrades = [];
+var bGrades = [];
+var cGrades = [];
+var dGrades = [];
+var fGrades = [];
+
+// //if (condition1) {
+//     block of code to be executed if condition1 is true
+// } else if (condition2) {
+//     block of code to be executed if the condition1 is false and condition2 is true
+// } else {
+//     block of code to be executed if the condition1 is false and condition2 is false
+// }
+
+for (var i = 0; i < scores.length; i++) {
+	  if (scores[i] >= 91) {
+	    aGrades++;
+	  }
+
+	  else if (grades[i]) >=81 {
+	  	bGrades++;
+	  }
+
+
+	  else if (grades[i]) >=71 {
+	  	cGrades++;
+	  }
+
+
+	  else if (grades[i]) >=61 {
+	  	dGrades++;
+	 
+	  }
+
+
+	  else if (grades[i]) >=50 {
+	  	fGrades++;
+	  }
+
+	}
+
+//var x = Math.min(f(foo), boundary);
+//var max = Math.max(...arr);
+
+var highest.grades = Math.max();
+var lowest.grades = Math.min();
+
+	  console.log(grades);
+	  console.log (highest.grades);
+	  console.log (lowest.grades);
+
